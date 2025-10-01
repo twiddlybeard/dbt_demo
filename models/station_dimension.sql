9a100959d39a592de2d866e1a38b51d0b5d80965
scott.mcalister@hhglobal.com <scott.mcalister@hhglobal.com>
2025-10-01 13:15:05 +0000

    station dimension model created

---
+++ models/station_dimension.sql
@@ -0,0 +1,10 @@
+with bike as (
+select  start_statio_id as station_id
+,      start_station_name as station_name
+,      start_lat
+,      start_lng
+from {{ source('demo', 'bike') }} 
+where lower(ride_id)<>'ride_id'
+
+)
+select * from bike
